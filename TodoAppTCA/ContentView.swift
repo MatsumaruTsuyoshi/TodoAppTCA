1bf12d761853d8c0a29eafb6897bf8fbdca1b447
tsuyoshi.matsumaru <tsuyoshi.matsumaru@uzabase.com>
2024-05-06 14:45:49 +0900

    Initial Commit

---
+++ TodoAppTCA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TodoAppTCA
+//
+//  Created by tsuyoshi.matsumaru on 2024/05/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TodoAppTCA/TodoAppTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppTCAApp.swift
+//  TodoAppTCA
+//
+//  Created by tsuyoshi.matsumaru on 2024/05/06.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
